f5597ba186f5bd287a3bfd6c7abee18125ba62a7
Matt Holland <matty.holland@gmail.com>
2024-10-05 18:00:33 +0200

    added delete and rename features

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,6 +15,10 @@ using AppLauncher;
 using AppLauncher.ShellClasses;
 using static AppLauncher.NativeMethods;
 using System.Configuration;
+using System.Windows.Controls;
+using AppLauncher.FileHelpers;
+using System.Collections;
+using System.Collections.Specialized;
 
 namespace AppLauncher
 {
@@ -55,13 +59,37 @@ namespace AppLauncher
                 else
                 {
                     _folderPath = ConfigurationManager.AppSettings["Folder"];
-                    EnumerateFiles();
+                    _folderPath = Environment.ExpandEnvironmentVariables(_folderPath);
+
+                    try
+                    {
+                        if (!Directory.Exists(_folderPath))
+                        {
+                            Directory.CreateDirectory(_folderPath);
+
+                            var defaultShortcuts = (NameValueCollection)ConfigurationManager.GetSection("DefaultShortcuts");
+
+                            if (defaultShortcuts != null)
+                            {
+                                foreach (var shortcutKey in defaultShortcuts.AllKeys)
+                                {
+                                    ShortCutHelper.CreateShortcut(shortcutKey, defaultShortcuts[shortcutKey], _folderPath, $"{shortcutKey}.lnk");
+                                }
+                            }
+                        }
+
+                        EnumerateFiles();
+
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"An error occurred deleting file.  {ex.Message}", "Application Error", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.Yes);
+                    }
+
                 }
             }
         }
 
-
-        // Minimize to system tray when application is minimized.
         protected override void OnStateChanged(EventArgs e)
         {
             //if (WindowState == WindowState.Minimized) this.Hide();
@@ -69,7 +97,6 @@ namespace AppLauncher
             base.OnStateChanged(e);
         }
 
-        // Minimize to system tray when application is closed.
         protected override void OnClosing(CancelEventArgs e)
         {
             // setting cancel to true will cancel the close request
@@ -120,15 +147,153 @@ namespace AppLauncher
 
                 foreach (var f in files)
                 {
+                    var fi = new FileInfo(f);
+
+                    // not a shortcut so create one
                     if (!f.EndsWith(".lnk", StringComparison.OrdinalIgnoreCase)) 
                     {
+                        var dialog = new PromptNameDialog();
+                        if (dialog.ShowDialog() == true)
+                        {
+                            var Name = dialog.ResponseText;
+                            var Parent = Directory.GetParent(fi.FullName).FullName;
+
+                            var newSCPath = Path.Combine(_folderPath, $"{Name}.lnk");
+
+                            if ((File.Exists(newSCPath)))
+                            {
+                                MessageBoxResult result = MessageBox.Show($"Shortcut already exists?  Overwrite", "Overwrite existing shortcut?", MessageBoxButton.YesNo, MessageBoxImage.Warning, MessageBoxResult.No);
+
+                                if (result == MessageBoxResult.Yes)
+                                {
+                                    File.Delete(newSCPath);
+                                    ShortCutHelper.CreateShortcut(Name, fi.FullName, _folderPath, $"{Name}.lnk");
+                                }
+                            }
+                            else
+                            {
+                                ShortCutHelper.CreateShortcut(Name, fi.FullName, _folderPath, $"{Name}.lnk");
+
+                                FilesCollection.Add(new FileSystemObjectInfo(new FileInfo(newSCPath)));
+                            }
+
+                        }
+
                         continue;
                     }
 
-                    var fi = new FileInfo(f);
-                    File.Copy(f, Path.Combine(_folderPath, fi.Name), true);
+                    var newPath = Path.Combine(_folderPath, fi.Name);
+
+                    if ((File.Exists(newPath)))
+                    {
+                        MessageBoxResult result = MessageBox.Show($"Shortcut already exists?  Overwrite", "Overwrite existing shortcut?", MessageBoxButton.YesNo, MessageBoxImage.Warning, MessageBoxResult.No);
+
+                        if(result == MessageBoxResult.Yes)
+                            File.Copy(f, Path.Combine(_folderPath, fi.Name), true);
+                    }
+                    else
+                    {
+                        File.Copy(f, Path.Combine(_folderPath, fi.Name), true);
+                        FilesCollection.Add(new FileSystemObjectInfo(new FileInfo(newPath)));
+                    }
                 }
             }
+        }
+
+        private void MenuItemDelete_Click(object sender, RoutedEventArgs e)
+        {
+            string caption = "Deletion";
+
+            MenuItem mi = sender as MenuItem;
+            if (mi != null)
+            {
+                ContextMenu cm = mi.CommandParameter as ContextMenu;
+                if (cm.DataContext is FileSystemObjectInfo)
+                {
+                    var theFile = (FileSystemObjectInfo)cm.DataContext;
+
+                    MessageBoxResult result = MessageBox.Show($"Are you sure you want to delete {theFile.Name}?", caption, MessageBoxButton.YesNo, MessageBoxImage.Warning, MessageBoxResult.No);
+
+                    if (result == MessageBoxResult.No)
+                        return;
+
+                    try
+                    {
+                        File.Delete(theFile.FilePath);
+                        FilesCollection.Remove(theFile);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"An error occurred deleting file.  {ex.Message}", caption, MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.Yes);
+                    }
+
+                    //Grid g = cm.PlacementTarget as Grid;
+                    //if (g != null)
+                    //{
+                    //Console.WriteLine(g.Background); // Will print red
+                    //}
+                }
+            }
+
+
+        }
+
+        private void MenuItemRename_Click(object sender, RoutedEventArgs e)
+        {
+            string caption = "Rename";
+
+            MenuItem mi = sender as MenuItem;
+            if (mi != null)
+            {
+                ContextMenu cm = mi.CommandParameter as ContextMenu;
+                if (cm.DataContext is FileSystemObjectInfo)
+                {
+                    var theFile = (FileSystemObjectInfo)cm.DataContext;
+
+                    MessageBoxResult result = MessageBox.Show($"Are you sure you want to delete {theFile.Name}?", caption, MessageBoxButton.YesNo, MessageBoxImage.Warning, MessageBoxResult.No);
+
+                    if (result == MessageBoxResult.No)
+                        return;
+
+                    try
+                    {
+                        File.Delete(theFile.FilePath);
+                        FilesCollection.Remove(theFile);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"An error occurred deleting file.  {ex.Message}", caption, MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.Yes);
+                    }
+
+                    //Grid g = cm.PlacementTarget as Grid;
+                    //if (g != null)
+                    //{
+                    //Console.WriteLine(g.Background); // Will print red
+                    //}
+                }
+            }
+
+
+        }
+
+        private void Window_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effects = DragDropEffects.Copy;
+            else
+                e.Effects = DragDropEffects.None;
+
+            e.Handled = true;
+        }
+
+        private void Window_DragOver(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effects = DragDropEffects.Copy;
+            else
+                e.Effects = DragDropEffects.None;
+
+            e.Handled = true;
         }
     }
 }
